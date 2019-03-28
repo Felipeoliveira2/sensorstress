64c18385399470f492f00ed8a1d98589d41d2238
felipe.oliveira <felipe.oliveira@corp.globo.com>
2019-03-28 11:00:53 -0300

    Stress sensor

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -1,10 +1,10 @@
 import eventlet
-from easysnmp import Session
+
 from st2reactor.sensor.base import Sensor
 
-class SnmpSensor(Sensor):
+class StressSensor(Sensor):
     def __init__(self, sensor_service, config):
-        super(SnmpSensor, self).__init__(sensor_service=sensor_service, config=config)
+        super(StressSensor, self).__init__(sensor_service=sensor_service, config=config)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
         self._stop = False
 
@@ -13,19 +13,12 @@ class SnmpSensor(Sensor):
 
     def run(self):
         while not self._stop:
-            self._logger.debug('SnmpSensor dispatching trigger...')
+            self._logger.debug('StressSensor dispatching trigger...')
         #############################################
-            session = Session(hostname='10.224.159.6', community='DataCenter', version=2)
-
-            system_items = session.walk('.1.3.6.1.2.1.25.4.2.1.2')
             payload_result_snmp = {'servicestatus': 0}
-            for item in system_items:
-                payload_result =  '{value}'.format(value=item.value)
-                if payload_result == "nginx":
-                    payload_result_snmp =  {'servicestatus': 1}
         #############################################
-            self.sensor_service.dispatch(trigger='snmpack.event1', payload=payload_result_snmp)
-            eventlet.sleep(30)
+            self.sensor_service.dispatch(trigger='sensorpack.event1', payload=payload_result_snmp)
+            eventlet.sleep(1)
 
     def cleanup(self):
         self._stop = True
